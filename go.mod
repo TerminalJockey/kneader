60f6be0eb1ac1b8f4869f6db4d6f78f8a89c669f
TerminalJockey <TerminalJockey@protonmail.com>
2023-02-01 17:00:08 -0700

    add vhost and portscan/fingerprint

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/TerminalJockey/kneader
 go 1.18
 
 require golang.org/x/net v0.5.0
+
+require github.com/projectdiscovery/wappalyzergo v0.0.80 // indirect
--- main.go
+++ main.go
@@ -11,9 +11,11 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
+	wappalyzer "github.com/projectdiscovery/wappalyzergo"
 	"golang.org/x/net/dns/dnsmessage"
 )
 
@@ -22,26 +24,43 @@ var (
 	wordlist string
 	resolver string
 	output   string
+	ports    string
+	Ports    []string
 	verbose  bool
+	vhosts   bool
 	timeout  int
 	threads  int
 
 	mutex = sync.RWMutex{}
 )
 
+type Host struct {
+	Address string
+	Domains []Domain
+}
+
+type Domain struct {
+	Name        string
+	Fingerprint []string
+}
+
 func init() {
 	flag.StringVar(&domain, "d", "", "domain to enumerate")
 	flag.StringVar(&wordlist, "w", "", "wordlist for bruteforce")
 	flag.StringVar(&resolver, "r", "", "resolver to query")
 	flag.StringVar(&output, "o", "", "output directory, defaults to no output")
+	flag.StringVar(&ports, "p", "", "ports to scan")
+	flag.BoolVar(&vhosts, "vhosts", false, "bruteforce subdomains/vhosts on found ips")
 	flag.BoolVar(&verbose, "v", false, "verbose output")
 	flag.IntVar(&timeout, "to", 1, "request timeout")
 	flag.IntVar(&threads, "t", 1, "threads")
 	flag.Parse()
+
+	Ports = strings.Split(ports, ",")
 }
 
 func main() {
-	if resolver == "" || wordlist == "" {
+	if resolver == "" || wordlist == "" || output == "" || ports == "" {
 		log.Println("check flags and rerun")
 		os.Exit(0)
 	}
@@ -61,9 +80,9 @@ func main() {
 		}
 	}
 
+	// initial dns enumeration to gather subdomains and ips
 	var wg sync.WaitGroup
 	throttle := make(chan int, threads)
-
 	tr := http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}
 	c := http.Client{Transport: &tr, Timeout: time.Second * time.Duration(timeout)}
 
@@ -71,14 +90,15 @@ func main() {
 	for scanner.Scan() {
 		throttle <- 1
 		wg.Add(1)
-		if verbose {
-			go func(scannerText string, wg *sync.WaitGroup, throttle chan int) {
-				defer wg.Done()
-				scandom := fmt.Sprintf("%s.%s", scannerText, domain)
-				ret := NameToIPs(scandom, c)
-				mutex.Lock()
-				fmt.Println(scandom)
-				fmt.Println(ret)
+		go func(scannerText string, wg *sync.WaitGroup, throttle chan int) {
+			defer wg.Done()
+			scandom := fmt.Sprintf("%s.%s", scannerText, domain)
+			ret := NameToIPs(scandom, c)
+			if verbose {
+				fmt.Printf("%s %s\n", scandom, ret)
+			}
+			mutex.Lock()
+			if len(ret) > 0 {
 				if outfileHandle != nil {
 					outfileHandle.WriteString(scandom + "|")
 					for x := range ret {
@@ -89,57 +109,201 @@ func main() {
 					}
 					outfileHandle.WriteString("\n")
 				}
-				mutex.Unlock()
-				<-throttle
-			}(scanner.Text(), &wg, throttle)
-			// scandom := fmt.Sprintf("%s.%s", scanner.Text(), domain)
-			// ret := NameToIPs(scandom)
-			// mutex.Lock()
-			// fmt.Println(scandom)
-			// fmt.Println(ret)
-			// if outfileHandle != nil {
-			// 	outfileHandle.WriteString(scandom + "|")
-			// 	for x := range ret {
-			// 		outfileHandle.WriteString(ret[x].String())
-			// 		if x != len(ret)-1 {
-			// 			outfileHandle.WriteString(",")
-			// 		}
-			// 	}
-			// 	outfileHandle.WriteString("\n")
-			// }
-			// mutex.Unlock()
-		} else {
-			go func(scannerText string, wg *sync.WaitGroup, throttle chan int) {
-				defer wg.Done()
-				scandom := fmt.Sprintf("%s.%s", scannerText, domain)
-				ret := NameToIPs(scandom, c)
-				mutex.Lock()
-				if len(ret) > 0 {
-					fmt.Printf("%s %s\n", scandom, ret)
-					if outfileHandle != nil {
-						outfileHandle.WriteString(scandom + "|")
-						for x := range ret {
-							outfileHandle.WriteString(ret[x].String())
-							if x != len(ret)-1 {
-								outfileHandle.WriteString(",")
-							}
-						}
-						outfileHandle.WriteString("\n")
-					}
-				}
-				mutex.Unlock()
-				<-throttle
-			}(scanner.Text(), &wg, throttle)
-		}
-
+			}
+			mutex.Unlock()
+			<-throttle
+		}(scanner.Text(), &wg, throttle)
 	}
 	wg.Wait()
+
+	outfileHandle.Seek(0, 0)
+
+	// vhost grind found ips
+	if vhosts {
+		var iplist []string
+		scanner = bufio.NewScanner(outfileHandle)
+		for scanner.Scan() {
+			ips := strings.Split(strings.Split(scanner.Text(), "|")[1], ",")
+			fmt.Println(ips)
+			for x := range ips {
+				var contains bool
+				for y := range iplist {
+					if ips[x] == iplist[y] {
+						contains = true
+					}
+				}
+				if !contains {
+					iplist = append(iplist, ips[x])
+				}
+			}
+		}
+		fmt.Println(iplist)
+
+		for x := range iplist {
+			wf.Seek(0, 0)
+			wfscanner := bufio.NewScanner(wf)
+			for wfscanner.Scan() {
+				throttle <- 1
+				wg.Add(1)
+				go func(scannerText string, wg *sync.WaitGroup, throttle chan int) {
+					defer wg.Done()
+					fulldomain := fmt.Sprintf("%s.%s", scannerText, domain)
+					newreq, err := http.NewRequest("GET", fmt.Sprintf("http://%s", iplist[x]), nil)
+					if err != nil {
+						log.Println(err)
+						<-throttle
+						return
+						// continue
+					}
+
+					newreq.Host = fulldomain
+					resp, err := c.Do(newreq)
+					if err != nil {
+						// log.Println(err)
+						<-throttle
+						return
+						// continue
+					}
+					defer resp.Body.Close()
+					if resp.StatusCode != 403 {
+						mutex.Lock()
+						fmt.Println(fulldomain)
+						fmt.Println(resp.Status, resp.ContentLength)
+						mutex.Unlock()
+					}
+					<-throttle
+				}(wfscanner.Text(), &wg, throttle)
+			}
+			wg.Wait()
+		}
+	}
+	outfileHandle.Seek(0, 0)
+
+	// port scan of found ips
+	scanner = bufio.NewScanner(outfileHandle)
+	for scanner.Scan() {
+		FingerprintDomain(scanner.Text(), c)
+	}
+
 	if outfileHandle != nil {
 		outfileHandle.Close()
 	}
 }
 
+func FingerprintDomain(line string, c http.Client) {
+	s := strings.Split(line, "|")
+	domain := s[0]
+	wapClient, err := wappalyzer.New()
+	if err != nil {
+		log.Println(err)
+	}
+	for x := range Ports {
+		var resp *http.Response
+		var url string
+		url = fmt.Sprintf("http://%s:%s", domain, Ports[x])
+		resp, err = c.Get(url)
+		if err != nil {
+			continue
+		}
+		if resp.StatusCode == 400 {
+			url = fmt.Sprintf("https://%s:%s", domain, Ports[x])
+			resp, err = c.Get(url)
+		}
+		if err != nil {
+			continue
+		}
+		defer resp.Body.Close()
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Println(err)
+		}
+
+		fingerprints, title := wapClient.FingerprintWithTitle(resp.Header, data)
+		if title == "" {
+			fmt.Println(strings.TrimSpace(url))
+		} else {
+			fmt.Println(strings.TrimSpace(url), strings.TrimSpace(title))
+		}
+
+		for a, _ := range fingerprints {
+			fmt.Printf("%s\n", a)
+		}
+	}
+}
+
+func IPToNames(ip string, c http.Client) (names []string) {
+	msg := dnsmessage.Message{
+		Header:    dnsmessage.Header{Response: false, Authoritative: true, RecursionDesired: true},
+		Questions: []dnsmessage.Question{{Name: dnsmessage.MustNewName(fmt.Sprintf("%s.in-addr.arpa.", ip)), Type: dnsmessage.TypePTR, Class: dnsmessage.ClassINET}},
+		Answers:   []dnsmessage.Resource{},
+	}
+	pmsg, err := msg.Pack()
+	if err != nil {
+		log.Println(err)
+		return names
+	}
+	b64msg := make([]byte, base64.RawURLEncoding.EncodedLen(len(pmsg)))
+	base64.RawURLEncoding.Encode(b64msg, pmsg)
+	qstr := fmt.Sprintf("https://%s/dns-query?dns=%s", resolver, b64msg)
+	req, err := http.NewRequest("GET", qstr, nil)
+	if err != nil {
+		log.Println(err)
+		return names
+	}
+	req.Header.Add("accept", "application/dns-message")
+	resp, err := c.Do(req)
+	if err != nil {
+		log.Println("doreq", err)
+		if err.Error() == "connectex: Only one usage of each socket address (protocol/network address/port) is normally permitted." {
+			time.Sleep(500 * time.Millisecond)
+			return IPToNames(ip, c)
+		}
+		return names
+	}
+	defer resp.Body.Close()
+	rcon, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+		return names
+	}
+	if verbose {
+		fmt.Println(rcon)
+	}
+	var p dnsmessage.Parser
+	_, err = p.Start(rcon)
+	if err != nil {
+		log.Println(err)
+		return names
+	}
+	p.SkipAllQuestions()
+
+	for {
+		h, err := p.AnswerHeader()
+		if err == dnsmessage.ErrSectionDone {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		switch h.Type {
+		case dnsmessage.TypePTR:
+			r, err := p.PTRResource()
+			if err != nil {
+				panic(err)
+			}
+			names = append(names, r.PTR.String())
+
+		default:
+			p.SkipAnswer()
+		}
+	}
+	return names
+}
+
 func NameToIPs(name string, c http.Client) (ips []net.IP) {
+	if len(name) > 255 {
+		return ips
+	}
 	var gotIPs []net.IP
 	msg := dnsmessage.Message{
 		Header:    dnsmessage.Header{Response: false, Authoritative: true, RecursionDesired: true},
@@ -153,17 +317,12 @@ func NameToIPs(name string, c http.Client) (ips []net.IP) {
 	}
 	b64msg := make([]byte, base64.RawURLEncoding.EncodedLen(len(pmsg)))
 	base64.RawURLEncoding.Encode(b64msg, pmsg)
-
-	// tr := http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}
-	// c := http.Client{Transport: &tr, Timeout: time.Second * time.Duration(timeout)}
-
 	qstr := fmt.Sprintf("https://%s/dns-query?dns=%s", resolver, b64msg)
 	req, err := http.NewRequest("GET", qstr, nil)
 	if err != nil {
 		log.Println(err)
 		return gotIPs
 	}
-
 	req.Header.Add("accept", "application/dns-message")
 	resp, err := c.Do(req)
 	if err != nil {
@@ -172,7 +331,6 @@ func NameToIPs(name string, c http.Client) (ips []net.IP) {
 			time.Sleep(500 * time.Millisecond)
 			return NameToIPs(name, c)
 		}
-		// if err == http.b
 		return gotIPs
 	}
 	defer resp.Body.Close()
@@ -200,7 +358,6 @@ func NameToIPs(name string, c http.Client) (ips []net.IP) {
 		if err != nil {
 			panic(err)
 		}
-
 		switch h.Type {
 		case dnsmessage.TypeA:
 			r, err := p.AResource()
